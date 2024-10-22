ae4bceb82e3b11576f31d73fda2f6e8bffa5e964
unknown <frenotama9@gmail.com>
2024-10-22 10:39:13 +0700

    first commit

--- resources/views/latihan/home.blade.php
+++ resources/views/latihan/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
-    <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
     <div class="container">
