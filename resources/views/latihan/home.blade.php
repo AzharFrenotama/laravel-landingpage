040cad1835d49cc0210a459942e3eb7e8fccce9c
unknown <frenotama9@gmail.com>
2024-10-22 10:33:14 +0700

    first commit

---
+++ resources/views/latihan/home.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Landing Page</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
+    <link rel="stylesheet" href="resources/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <nav class="atas">
+            <div class="awal">
+                <div class="logo"><img src="Asset/Logo.png" alt=""></div>
+            </div>
+            <ul class="header">
+                <li><a href="">Home</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Feature</a></li>
+                <li><a href="">Product</a></li>
+                <li><a href="">Testimonial</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+            
+            <div class="kanan">
+                <button class="login">Login</button>
+                <button class="signup">Sign Up</button>
+            </div>
+        </nav>
+    </div>
+
+    <main>
+        <div class="utama">
+            <h1 class="les">Lesson and Insight</h1>
+            <h1 class="from">from 8 years</h1>
+            <p class="grow">Where to grow your business as a photographer: site or social media?</p>
+            <button class="regist">Register</button>
+            <div class="ilus"><img src="Asset/Illustration.png" alt=""></div>
+        </div>
+
+        <div class="client">
+            <div class="title1">
+                <h1>Our clients</h1>
+                <p class="txtclient">We have been working with some Fortune 500+ clients</p>
+            </div>
+            <div class="icon">
+                <img src="Asset/icon tengah.png">
+                <img src="Asset/Logo-1.png">
+                <img src="Asset/Logo-2.png">
+                <img src="Asset/Logo-3.png">
+                <img src="Asset/Logo-4.png">
+                <img src="Asset/Logo-5.png">
+                <img src="Asset/Logo-6.png">
+            </div>
+        </div>
+
+        <div class="manage">
+            <div class="title2">
+                <h1>Manage your entire community</h1>
+                <h1 class="sys">in a single system</h1>
+                <p class="who">who is Nexcent suitable for?</p>
+            </div>
+
+            <div class="icon2">
+                <img src="Asset/Iconn (1).png" alt="">
+                <img src="Asset/Iconn (2).png" alt="">
+                <img src="Asset/Iconn (3).png" alt="">
+            </div>
+
+            <div class="txt3">
+                <h2 >Membership</h2>
+                <h2 class="nat" >National </h2>
+                <h2 class="club" >Clubs And</h2>
+            </div>
+
+            <div class="br">
+                <h2 class="org">Organisations</h2>
+                <h2 class="ass">Associations</h2>
+                <h2 class="grp">Groups</h2>
+            </div>
+
+            <div class="txt3l">
+                <p class="ket">Our membership management software provides full automation of membership renewals and payments</p>
+                <p class="ket2">Our membership management software provides full automation of membership renewals and payments</p>
+                <p class="ket3">Our membership management software provides full automation of membership renewals and payments</p>
+            </div>
+
+            <div class="kedua">
+                <h1 class="uns">The unseen of spending three</h1>
+                <h1 class="pxl">years at Pixelgrade</h1>
+                <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed <br> accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed <br> porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam <br> quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                <button class="lrn">Learn More</button>
+                <div class="ilus2"><img src="Asset/Frame 35.png" alt=""></div>
+            </div>
+        </div>
+
+        <div class="empaticn">
+            <div class="tgh">
+                <h2 class="help">Helping a local</h2>
+                <h2 class="bsn">business reinvent itself</h2>
+                <p class="we">We reached here with our hard work and dedication</p>
+            </div>
+
+            <div class="tgh2">
+                <h2 class="jml">2,245,341</h2>
+                <p class="member">Members</p>
+                <h2 class="jml2">828,867</h2>
+                <p class="evnt">Event Bookings</p>
+                <h2 class="jml3">46,328</h2>
+                <p class="clb">Clubs</p>
+                <h2 class="jml4">1,926,436</h2>
+                <p class="pay">Payments</p>
+                <img class="orag" src="Asset/org.png" alt="">
+                <img class="tgn" src="Asset/tgn.png" alt="">
+                <img class="jari" src="Asset/jari.png" alt="">
+                <img class="dmp" src="Asset/dompet.png" alt="">
+            </div>
+        </div>
+
+        <div class="ketiga">
+            <h2 class="desig">How to design your site footer like</h2>
+            <h2 class="did">we did</h2>
+            <p class="lorem2">Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt <br> molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at <br> libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta <br> nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer <br> in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi <br> ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.</p>
+            <button class="lrn2">Learn More</button>
+            <div class="ilus3"><img src="Asset/pana.png" alt=""></div>
+        </div>
+
+        <div class="keempat">
+            <div class="gambaran">
+                <p class="lorem3">Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus <br> tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida <br> enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie <br> mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse <br> eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, <br> vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum <br> id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
+                <h3 class="tim">Tim Smith</h3>
+                <p class="british">British Dragon Boat Racing Association</p>
+                <h3 class="meet">Meet all customers</h3>
+                <i class="ri-arrow-right-line"></i>
+                <img class="gambar" src="Asset/image 9.png" alt="">
+                <img class="logos" src="Asset/logo warna.png" alt="">
+                <img class="logos2" src="Asset/Logo-1.png" alt="">
+                <img class="logos3" src="Asset/Logo-2.png" alt="">
+                <img class="logos4" src="Asset/Logo-3.png" alt="">
+                <img class="logos5" src="Asset/Logo-4.png" alt="">
+                <img class="logos6" src="Asset/Logo-5.png" alt="">
+            </div>
+        </div>
+
+
+        <div class="psfl">
+            <h1>Pellentesque suscipit<br>fringilla libero eu.</h1>
+            <button>Get a Demo →</button>
+          </div>
+    
+        </div>
+    </main>
+
+    <footer>
+        <div class="knn">
+            <div class="pp">
+            <img src="gmbr/Icon.png" alt=""> <h1>Nexcent</h1>
+            </div>
+            <p>Copyright © 2020 Nexcent ltd.<br>All rights reserved</p>
+        </div>
+        <div class="tgh">
+            <div class="deret1">
+            <h3>Company</h3>
+            <ul class="bs1">
+                <li>About us</li>
+                <li>Blog</li>
+                <li>Contact us</li>
+                <li>Pricing</li>
+                <li>Testimonials</li>
+            </ul>
+            </div>
+            <div class="deret2">
+            <h3>Supoort</h3>
+            <ul class="bs2">
+                <li>Help center</li>
+                <li>Terms of Service</li>
+                <li>Legal</li>
+                <li>Privacy policy</li>
+                <li>Status</li> 
+            </ul>
+            </div>
+        </div>
+        <div class="krr">
+            <h3>Stay up to date</h3>
+            <input type="text" placeholder="Your email address">
+        </div>
+    </footer>
+
+</body>
+</html>
